0d6821d98f988744c3e4227155bcfceddbb50c08
RainierBarrett <rainier.barrett@gmail.com>
2019-12-16 12:58:41 -0500

    Update NN structure for better agreement.

--- htf/models/example-models/ann-learned/build_and_run.py
+++ htf/models/example-models/ann-learned/build_and_run.py
@@ -42,10 +42,8 @@ def make_train_graph(NN, N_hidden_nodes, N_hidden_layers, r_cut, system):
     # get sorted neighbor list
     nlist = graph.nlist[:, :, :3]  
     # get the interatomic radii
+    cost_tracker = tf.Variable(0.0, name='cost')
     r = hoomd.htf.graph_builder.safe_norm(nlist, axis=2)
-    nn_r = tf.Variable(tf.zeros(shape=[64, NN]), trainable=False)
-    nn_r.assign(r)
-    histo3 = tf.summary.histogram('neighbor radius', nn_r)
     r_inv = hoomd.htf.graph_builder.safe_div(1., r)
     print('r_inv shape: {}'.format(r_inv.shape))
     # make weights tensors, using our number of hidden nodes
@@ -53,12 +51,14 @@ def make_train_graph(NN, N_hidden_nodes, N_hidden_layers, r_cut, system):
     r_inv = tf.reshape(r_inv, shape=[-1, 1], name='r_inv')
 
     keep_prob = tf.placeholder(dtype=tf.float32, name='keep_prob', shape=[])
+    hoomd_energy = tf.placeholder(dtype=tf.float32, name='hoomd_energy', shape=[])
     # specify the network structure
     output_layer = build_neural_network(r_inv, keep_prob,
                                         N_hidden_nodes, N_hidden_layers,
                                         activation_func=tf.nn.tanh)
     nn_energies = tf.reshape(output_layer, shape=[-1, NN])  # recover structure
     print('nn_energies shape: {}'.format(nn_energies.shape))
+    nn_tot_en = tf.reduce_sum(nn_energies)
     # calculate the forces
     calculated_energies = tf.reduce_sum(nn_energies, axis=1,
                                         name='calculated_energies')
@@ -67,25 +67,25 @@ def make_train_graph(NN, N_hidden_nodes, N_hidden_layers, r_cut, system):
     calculated_forces = graph.compute_forces(nn_energies)
     print('calculated_forces shape is: {}'.format(calculated_forces.shape))
     # compare calculated forces to HOOMD's forces
-    cost = tf.losses.mean_squared_error(graph.forces, calculated_forces)
+    force_cost = tf.losses.mean_squared_error(graph.forces, calculated_forces)
+    en_cost =  tf.losses.mean_squared_error(nn_tot_en, hoomd_energy)
+    cost = force_cost#force_cost + en_cost
     # need to minimize the cost
     optimizer = tf.train.AdamOptimizer(learning_rate=0.005).minimize(cost)
+    update_cost = tf.assign(cost_tracker, cost)
     # print summaries for tensorboard
     tf.summary.scalar('cost', cost)
-    histo = tf.summary.histogram('calculated forces', calculated_forces)
-    histo2 = tf.summary.histogram('hoomd forces', graph.forces)
     # print cost for a more granular plot
-    printer2 = tf.Print(cost, [cost], summarize=100, message="cost is: ")
     # check = tf.add_check_numerics_ops()
     graph.save(model_directory=model_dir,
-               out_nodes=[optimizer, histo, histo2, histo3, printer2],
+               out_nodes=[optimizer, update_cost],
                move_previous=False)
 
 minval = -1.
 maxval = 1.
-NN = 63
-N_hidden_nodes = 20
-N_hidden_layers = 1
+NN = 128
+N_hidden_nodes = 30
+N_hidden_layers = 2
 
 hoomd.context.initialize('--mode=cpu')#('--mode=gpu')
 rcut = 3.0
@@ -93,11 +93,12 @@ sqrt_N = int(sqrt(N))
 system = hoomd.init.create_lattice(unitcell=hoomd.lattice.sq(a=2.0),
                                    n=[sqrt_N, sqrt_N])
 make_train_graph(NN, N_hidden_nodes, N_hidden_layers, rcut, system)
-nlist = hoomd.md.nlist.cell(check_period=1)
+nlist = hoomd.md.nlist.cell()
 # basic LJ forces from HOOMD
 lj = hoomd.md.pair.lj(rcut, nlist)
 lj.pair_coeff.set('A', 'A', epsilon=1.0, sigma=1.0)
 hoomd.md.integrate.mode_standard(dt=0.005)
+logger = hoomd.analyze.log(filename=None, quantities=['potential_energy'], period=10)
 hoomd.md.integrate.langevin(group=hoomd.group.all(), kT=1.0, seed=42)
 # hoomd.md.integrate.nve(group=hoomd.group.all()).
 # randomize_velocities(kT=1.2, seed=42)
@@ -105,13 +106,13 @@ hoomd.md.integrate.langevin(group=hoomd.group.all(), kT=1.0, seed=42)
 hoomd.run(4000)
 
 with hoomd.htf.tfcompute(model_dir,
-                         _mock_mode=False,
                          write_tensorboard=True) as tfcompute:
     # attach the trainable model
     tfcompute.attach(nlist,
                      r_cut=rcut,
                      save_period=100,
                      period=10,
-                     feed_dict=dict({'keep_prob:0': 0.8}))
+                     feed_dict=dict({'keep_prob:0': 0.8,
+                                     'hoomd_energy:0': logger.query('potential_energy')}))
     # train on specified number of timesteps
     hoomd.run(N_STEPS)
